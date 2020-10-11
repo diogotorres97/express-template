1e141b96781d85eef74f23945f9fc47d82151457
António Almeida <theantonioalmeida@gmail.com>
2020-10-11 22:22:12 +0200

    Add base Actions CI with linter (#6)
    * add sample CI file with linter
    
    * move ci to correct directory
    
    * add cd into server directory
    
    * add package-lock.json for ci install
    
    * fix linter errors
    
    * remove 8 and 10 node versions from ci
    
    * rename 'build' job to 'lint'

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -9,11 +9,9 @@ const auth = require('./auth');
 const todos = require('./todos');
 const todoItems = require('./todoItems');
 
-
 router.use('/api/', auth);
 router.use('/api/', passport.authenticate('jwt', { session: false }));
 router.use('/api/', todos);
 router.use('/api/', todoItems);
-
 
 module.exports = router;
